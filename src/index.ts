7c3993b70e5b49cd50cab73a4be1f25ac2bfff93
claramarsango <claraxmarsango@gmail.com>
2023-02-05 21:35:21 +0100

    remove assertion in appContainer variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import App from './components/App/App.js';
 import { pokemonsList, getPokemon } from './components/get-pokemons-list.js';
 
-const appContainer = document.body as HTMLElement;
+const appContainer = document.body;
 const app = new App(appContainer, pokemonsList);
 
 getPokemon();

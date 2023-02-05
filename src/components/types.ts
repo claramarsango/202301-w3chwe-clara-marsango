c2d288099f18be2d8258034349cbbf7146b6d398
claramarsango <claraxmarsango@gmail.com>
2023-02-05 15:54:34 +0100

    create pokemon interface

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,7 +1,25 @@
-interface ComponentStructure {
+export interface ComponentStructure {
   domElement: HTMLElement;
   render: () => void;
   addListeners?: () => void;
 }
 
-export default ComponentStructure;
+export interface Pokemon {
+  id: number;
+  name: string;
+  baseExperience: number;
+  height: number;
+  weight: number;
+  types: { type: { name: string } }[];
+  sprites: {
+    versions: {
+      'generation-v': {
+        'black-white': {
+          animated: {
+            front_default: string;
+          };
+        };
+      };
+    };
+  };
+}
